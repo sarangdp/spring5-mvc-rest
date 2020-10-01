1a484cfa967a2ef04901aa550aad49ca0d1fc2ae
Sarang <sarangdp@gmail.com>
2020-10-01 08:41:12 -0400

    Added Category Domain and Repository class

--- src/main/java/guru/springfamework/Spring5MvcRestApplication.java
+++ src/main/java/guru/springfamework/Spring5MvcRestApplication.java
@@ -1,4 +1,4 @@
-package guru.springfamework.spring5mvcrest;
+package guru.springfamework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
