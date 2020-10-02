a01385caa0183dd545871f43f5f1136e3ddb0e29
Sarang <sarangdp@gmail.com>
2020-10-01 22:03:15 -0400

    MapStruct implementation.

---
+++ src/test/java/guru/springfamework/mappers/CategoryMapperTest.java
@@ -0,0 +1,25 @@
+package guru.springfamework.mappers;
+
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.domain.Category;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CategoryMapperTest {
+
+    private CategoryMapper mapper = CategoryMapper.INSTANCE;
+
+    @Test
+    public void categoryToCategoryDto() {
+        Category category = new Category();
+        category.setName("Drinks");
+        category.setId(2L);
+
+        CategoryDTO categoryDTO = mapper.categoryToCategoyDto(category);
+
+        assertEquals(Long.valueOf(2L), categoryDTO.getId());
+        assertEquals("Drinks", categoryDTO.getName());
+    }
+
+}
